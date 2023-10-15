284e97a5138dd9ecf8abb15c313b08f239658464
shubhamverma2000 <[vermashubham0608@gmail.com]>
2023-10-15 19:30:08 +0530

    weather-app-version

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "b465e8a20cef384ffe203c41ad4fea0d",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
